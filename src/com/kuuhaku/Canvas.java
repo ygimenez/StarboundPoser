4fa2f59b7cb130f7ea8d835cec6410717129f6ee
Yago <yagogimenez1@hotmail.com>
2019-11-07 13:49:49 -0200

    Done

--- src/com/kuuhaku/Canvas.java
+++ src/com/kuuhaku/Canvas.java
@@ -8,6 +8,7 @@ import static com.kuuhaku.Poser.*;
 public class Canvas extends JComponent {
 
 	//298, 286
+	//12, 83
 	@Override
 	public void paint(Graphics g) {
 		super.paintComponents(g);
@@ -16,11 +17,18 @@ public class Canvas extends JComponent {
 
 		g2d.clearRect(0, 0, 512, 512);
 		g2d.drawImage(BODY, 0, 0, null);
-
+		//START ARM ALIGNMENT
 		g2d.translate(298, 286);
-		g2d.rotate(Math.toRadians(armAngle));
+		g2d.rotate(Math.toRadians(armAngle + 90));
+		//START WEAPON ALIGNMENT
+		g2d.translate(12, 83);
+		g2d.rotate(Math.toRadians(wpnAngle - 90));
+		g2d.drawImage(WEAPON, (centerWpn ? -WEAPON.getWidth() / 2 : -WEAPON.getWidth()) + (Math.round(wpnOffset[0] * 10) * 11), (centerWpn ? -WEAPON.getHeight() / 2 : -WEAPON.getHeight()) + (Math.round(wpnOffset[1] * 10) * 11), null);
+		//END WEAPON ALIGNMENT
+		g2d.rotate(-Math.toRadians(wpnAngle + 270));
+		g2d.translate(-12, -83);
+		//END ARM ALIGNMENT
 		g2d.drawImage(ARM, -298, -286, null);
-		g2d.rotate(-Math.toRadians(armAngle));
-		g2d.translate(0, 0);
+		g2d.rotate(-Math.toRadians(armAngle - 90));
 	}
 }
--- src/com/kuuhaku/Poser.java
+++ src/com/kuuhaku/Poser.java
@@ -2,6 +2,7 @@ package com.kuuhaku;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -12,22 +13,30 @@ import java.util.Objects;
 public class Poser extends JFrame {
 	private Canvas canvas;
 	private JPanel container;
-	private JSpinner wpnController;
+	private JSpinner wpnAngController;
 	private JSpinner armController;
-	private Graphics2D g2d = new BufferedImage(512, 512, BufferedImage.TYPE_INT_ARGB).createGraphics();
+	private JSpinner wpnXOffset;
+	private JCheckBox centerWeapon;
+	private JSpinner wpnYOffset;
+	private JButton chooseWpn;
 	static BufferedImage ARM;
 	static BufferedImage BODY;
-	static int armAngle;
-	static int wpnAngle;
+	static BufferedImage WEAPON;
+	static int armAngle = -90;
+	static int wpnAngle = -10;
+	static float[] wpnOffset = new float[]{0, -1.8f};
+	static boolean centerWpn = true;
 
 	Poser() {
 		try {
 			ARM = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResource("com/kuuhaku/assets/arm.png")));
 			BODY = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResource("com/kuuhaku/assets/body.png")));
+			WEAPON = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResource("com/kuuhaku/assets/weapon.png")));
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(this, "Oops, an error has ocurred: " + e);
 			ARM = null;
 			BODY = null;
+			WEAPON = null;
 		}
 
 		add(container);
@@ -44,18 +53,58 @@ public class Poser extends JFrame {
 			canvas.repaint();
 		});
 
-		wpnController.addChangeListener(e -> {
-			wpnAngle = (int) wpnController.getValue();
+		wpnAngController.addChangeListener(e -> {
+			wpnAngle = (int) wpnAngController.getValue();
 			canvas.repaint();
 		});
 
-		setSize(700, 540);
+		wpnXOffset.addChangeListener(e -> {
+			wpnOffset[0] = (Float) wpnXOffset.getValue();
+			canvas.repaint();
+		});
+
+		wpnYOffset.addChangeListener(e -> {
+			wpnOffset[1] = (Float) wpnYOffset.getValue();
+			canvas.repaint();
+		});
+
+		centerWeapon.addActionListener(e -> {
+			centerWpn = centerWeapon.isSelected();
+			canvas.repaint();
+		});
+
+		chooseWpn.addActionListener(e -> {
+			JFileChooser chooser = new JFileChooser();
+			chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			chooser.setFileFilter(new FileNameExtensionFilter("Image file", "jpeg", "jpg", "png"));
+			if (chooser.showSaveDialog(null) == 	JFileChooser.APPROVE_OPTION) {
+				try {
+					Image chosenFile = ImageIO.read(chooser.getSelectedFile());
+					chosenFile = chosenFile.getScaledInstance(Math.round(chosenFile.getWidth(null) * 11.8f), Math.round(chosenFile.getHeight(null) * 11.8f), Image.SCALE_SMOOTH);
+					WEAPON = new BufferedImage(chosenFile.getWidth(null), chosenFile.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+					Graphics2D g2d = WEAPON.createGraphics();
+					g2d.drawImage(chosenFile, 0, 0, null);
+					g2d.dispose();
+				} catch (IOException ex) {
+					JOptionPane.showMessageDialog(this, "Oops, an error has ocurred: " + ex);
+				}
+			}
+			canvas.repaint();
+		});
+
+		setTitle("Starbound Poser");
+		setSize(750, 540);
+		setLocationRelativeTo(null);
 		setResizable(false);
 		setVisible(true);
 		canvas.repaint();
 	}
 
 	private void createUIComponents() {
-		canvas = new Canvas();
+		armController = new JSpinner(new SpinnerNumberModel(-90, -360, 360, 1));
+		wpnAngController = new JSpinner(new SpinnerNumberModel(-10, -360, 360, 1));
+		wpnXOffset = new JSpinner(new SpinnerNumberModel(Float.valueOf(0), Float.valueOf(-256), Float.valueOf(256), Float.valueOf(0.1f)));
+		wpnYOffset = new JSpinner(new SpinnerNumberModel(Float.valueOf(-1.8f), Float.valueOf(-256), Float.valueOf(256), Float.valueOf(0.1f)));
 	}
 }
